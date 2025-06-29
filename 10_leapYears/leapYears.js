f5f118f690c9f402c571a3d29738837b4b011865
August <76185140+Augustina1995@users.noreply.github.com>
2025-06-29 15:50:35 +0300

    practice

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    if(year % 4 === 0 && (year % 400 === 0 || year % 100 !== 0)) {
+        return true
+    } else {
+        return false
+    }
 };
+
+console.log(leapYears(2000)); //true
+console.log(leapYears(1984)); //true
+console.log(leapYears(1900)); //false
+console.log(leapYears(1600)); //true
+
 
 // Do not edit below this line
 module.exports = leapYears;
