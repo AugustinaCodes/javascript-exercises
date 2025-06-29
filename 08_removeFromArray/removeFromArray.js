86d9e1e6990a740fb0862e9b7cb5513eba59ee74
August <76185140+Augustina1995@users.noreply.github.com>
2025-06-29 14:55:49 +0300

    array practice

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, item) {
+  const itemIndex = arr.indexOf(item);
+  arr.splice(itemIndex, 1);
+  return arr;
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
