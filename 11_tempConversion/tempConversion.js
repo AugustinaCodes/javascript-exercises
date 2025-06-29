6f5c82335a314b78e4becb3c9b06abbda3bf0586
August <76185140+Augustina1995@users.noreply.github.com>
2025-06-29 15:50:19 +0300

    js exercise practice

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  return (num - 32) * 5/9
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  return (num * 9/5) + 32
 };
+
+console.log(convertToCelsius(32));
+console.log(convertToFahrenheit(0));
+
 
 // Do not edit below this line
 module.exports = {
