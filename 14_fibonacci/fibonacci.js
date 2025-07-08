601f2a53c0fabc405338a543bb77ded613107098
August <76185140+Augustina1995@users.noreply.github.com>
2025-07-08 12:56:56 +0300

    practice

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let arr = []
+
+    for (let i = 1; i <= number; i++) {
+        console.log(i);
+        
+        arr.push(i)
+    }
+
+    return arr
 
 };
+
+console.log(fibonacci(1));
+
 
 // Do not edit below this line
 module.exports = fibonacci;
