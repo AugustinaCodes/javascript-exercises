e3783c1fea76930f6c03c161a5ec90e8a19cf2db
August <76185140+Augustina1995@users.noreply.github.com>
2025-07-08 12:45:55 +0300

    js practice for odin

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((sum, n) => sum + n, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, number) => product * number)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number === 0) {
+    return 1
+  }
+  let answer = 1
+  for (let i = 1; i <= number; i++) {
+    answer *= i
+  }
+  return answer
 };
 
 // Do not edit below this line
